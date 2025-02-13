ef75035893a59b20e3ac643df019b607f53af5c1
somone014 <girl826@g.ucla.edu>
2025-02-13 11:56:20 -0800

    download change

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Winter 2025 | PIC 16B | Group Project | Sarah Burnett
 Group members: Sophia Hatsell, Caroline Ives, May Jiang
+
+
+Necessary Packages: Manim, Librosa, Requests
--- preview_request.py
+++ preview_request.py
@@ -4,7 +4,8 @@ import requests
 base_url = "https://api.deezer.com/search"
 
 
-query = "Shape of You" #Developer Note: Set to change
+query = input("Enter a song title: ") # Developer Note: See if you can add album/artist to narrow search results, 
+
 
 search = requests.get(f"{base_url}?q={query}")
 
@@ -12,20 +13,24 @@ search = requests.get(f"{base_url}?q={query}")
 if search.status_code == 200:
     data = search.json()
 
-    #Check if data is empty
+    #Check if search data is empty
     if data['data']:
         track = data['data'][0] # First song in the search
+        
+        preview_url = track['preview'] # Get preview url
+        title = track['title']
+        artist = track['artist']['name']
+        
+        preview_response = requests.get(preview_url) # Retrieve preview from url
 
-        preview_url = track['preview'] #Get preview url
-        preview_response = requests.get(preview_url)
         # Check if preview URL was retrieved successfully
         if preview_response.status_code == 200:
             # Save the preview as an MP3 file
-            output_file = f"{query}_preview.mp3"
-            with open(output_file, 'wb') as f:
+            song_preview_file = f"{title}_{artist}_preview.mp3" # Maybe change to be the name of the song title, artist?
+            with open(song_preview_file, 'wb') as f:
                 f.write(preview_response.content)
-            print(f"The 30-second preview has been saved as {output_file}.")
+            print(f"The 30-second preview has been saved as {song_preview_file}.")
         else:
-            print("Failed to download the preview audio.")
+            print(f"Failed to download the preview audio. Status Code: {preview_response.status_code}.")
 else:
-    print(f"Failed to fetch track info. Status Code: {response.status_code}")
+    print(f"Failed to search. Status Code: {search.status_code}")
