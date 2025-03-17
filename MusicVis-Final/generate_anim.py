06974c45543e0f05b3f3fea65ca5816f0461f46f
smhatsell1 <smhatsell1@gmail.com>
2025-03-17 00:30:59 -0700

    Comments/docstring generate_anim.py

--- MusicVis-Final/generate_anim.py
+++ MusicVis-Final/generate_anim.py
@@ -7,22 +7,31 @@ import subprocess
 from video_anim import circleAnim  
 
 def render_manim_scene(song_path):
-    # Ensure the output directory exists
+    """Render a Manim animation scene based on a given 
+    song path and save the output as an MP4 file.
+    @param: 
+        song_path (str): The path to the song file
+    
+    @returns: 
+        str: The path to the generated MP4 file containing the animation.
+    """
+
+    # ensure the output directory exists
     if not os.path.exists('static/animations'):
         os.makedirs('static/animations')
 
-    # Extract song title from filename
+    # extract song title from filename
     output_directory = os.path.join(os.path.dirname(__file__), 'static/animations')
     song_title = os.path.basename(song_path).split("_")[0]
     output_file = os.path.join(output_directory, f"{song_title}.mp4")
 
-    # Define the output file path
+    # define the output file path
     song_title = song_path.split("/")[2].split("_")[0]
     output_file = os.path.join(output_directory, f"{song_title}.mp4")
 
     os.environ["SONG_PATH"] = song_path
 
-    # Construct the Manim command with the correct output path
+    # construct the Manim command with the correct output path
     command = [
         "manim", 
         "-ql",  # quality: low
@@ -31,13 +40,13 @@ def render_manim_scene(song_path):
         "-o", output_file  # Output path for the animation
     ]
     
-    # Print the command for debug purposes
-    print("Running Manim command:", " ".join(command))
+    # print the command for debug purposes
+    # print("Running Manim command:", " ".join(command)) #UNCOMMENT FOR DEBUG
     
-    # Run the command
+    # run the command
     subprocess.run(command)
 
-
-    return output_file  # Return the final MP4 file path
+    # return the final MP4 file path
+    return output_file
 
 
